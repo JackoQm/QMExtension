016557119215042ce49ef7cfdb6714d93cd97469
Jacko Qm <userqm@163.com>
2017-07-17 15:59:43 +0800

    v1.0.1
    补充了UIView的origin属性拓展，并增加了bounds属性拓展

--- QMExtension/UIView+QMExtension.h
+++ QMExtension/UIView+QMExtension.h
@@ -10,16 +10,46 @@
 
 @interface UIView (QMExtension)
 
+/**
+ 该 View 的 Frame 的左上角 x 坐标，可以直接修改
+ */
+@property(nonatomic) CGFloat qm_x;
 
 /**
- 该View的Frame的宽度（width），可以直接修改
+ 该 View 的 Frame 的左上角 y 坐标，可以直接修改
+ */
+@property(nonatomic) CGFloat qm_y;
+
+/**
+ 该 View 的 Frame 的宽度（width），可以直接修改
  */
 @property(nonatomic) CGFloat qm_width;
 
 
 /**
- 该View的Frame的高度（height），可以直接修改
+ 该 View 的 Frame 的高度（height），可以直接修改
  */
 @property(nonatomic) CGFloat qm_height;
+
+/**
+ 该 View 的 Bounds 的左上角 x 坐标，可以直接修改
+ */
+@property(nonatomic) CGFloat qm_boundsX;
+
+/**
+ 该 View 的 Bounds 的左上角 y 坐标，可以直接修改
+ */
+@property(nonatomic) CGFloat qm_boundsY;
+
+/**
+ 该 View 的 Bounds 的宽度（width），可以直接修改
+ */
+@property(nonatomic) CGFloat qm_boundsWidth;
+
+
+/**
+ 该 View 的 Bounds 的高度（height），可以直接修改
+ */
+@property(nonatomic) CGFloat qm_boundsHeight;
 
 @end
--- QMExtension/UIView+QMExtension.m
+++ QMExtension/UIView+QMExtension.m
@@ -7,9 +7,49 @@
 //
 
 #import "UIView+QMExtension.h"
+#import <Foundation/Foundation.h>
 
 @implementation UIView (QMExtension)
 
+/**
+ 该 View 的 Frame 的左上角 x 坐标，可以直接修改
+ */
+- (CGFloat)qm_x
+{
+    return CGRectGetMinX(self.frame);
+}
+
+- (void)setQm_x:(CGFloat)qm_x
+{
+    if (self.qm_x == qm_x) {
+        return;
+    }
+    CGRect rect = self.frame;
+    rect.origin.x = qm_x;
+    self.frame = rect;
+}
+
+/**
+ 该 View 的 Frame 的左上角 y 坐标，可以直接修改
+ */
+- (CGFloat)qm_y
+{
+    return CGRectGetMinY(self.frame);
+}
+
+- (void)setQm_y:(CGFloat)qm_y
+{
+    if (self.qm_y == qm_y) {
+        return;
+    }
+    CGRect rect = self.frame;
+    rect.origin.y = qm_y;
+    self.frame = rect;
+}
+
+/**
+ 该 View 的 Frame 的宽度（width），可以直接修改
+ */
 - (CGFloat)qm_width
 {
     return CGRectGetWidth(self.frame);
@@ -17,11 +57,17 @@
 
 - (void)setQm_width:(CGFloat)qm_width
 {
+    if (self.qm_width == qm_width) {
+        return;
+    }
     CGRect rect = self.frame;
     rect.size.width = qm_width;
     self.frame = rect;
 }
 
+/**
+ 该 View 的 Frame 的宽度（width），可以直接修改
+ */
 - (CGFloat)qm_height
 {
     return CGRectGetHeight(self.frame);
@@ -29,9 +75,84 @@
 
 - (void)setQm_height:(CGFloat)qm_height
 {
+    if (self.qm_height == qm_height) {
+        return;
+    }
     CGRect rect = self.frame;
     rect.size.height = qm_height;
     self.frame = rect;
+}
+
+/**
+ 该 View 的 Bounds 的左上角 x 坐标，可以直接修改
+ */
+- (CGFloat)qm_boundsX
+{
+    return CGRectGetMinX(self.bounds);
+}
+
+- (void)setQm_boundsX:(CGFloat)qm_boundsX
+{
+    if (self.qm_boundsX == qm_boundsX) {
+        return;
+    }
+    CGRect rect = self.bounds;
+    rect.origin.x = qm_boundsX;
+    self.bounds = rect;
+}
+
+/**
+ 该 View 的 Bounds 的左上角 y 坐标，可以直接修改
+ */
+- (CGFloat)qm_boundsY
+{
+    return CGRectGetMinY(self.bounds);
+}
+
+- (void)setQm_boundsY:(CGFloat)qm_boundsY
+{
+    if (self.qm_boundsY == qm_boundsY) {
+        return;
+    }
+    CGRect rect = self.bounds;
+    rect.origin.y = qm_boundsY;
+    self.bounds = rect;
+}
+
+/**
+ 该 View 的 Bounds 的宽度（width），可以直接修改
+ */
+- (CGFloat)qm_boundsWidth
+{
+    return CGRectGetWidth(self.bounds);
+}
+
+- (void)setQm_boundsWidth:(CGFloat)qm_boundsWidth
+{
+    if (self.qm_boundsWidth == qm_boundsWidth) {
+        return;
+    }
+    CGRect rect = self.bounds;
+    rect.size.width = qm_boundsWidth;
+    self.bounds = rect;
+}
+
+/**
+ 该 View 的 Bounds 的高度（height），可以直接修改
+ */
+- (CGFloat)qm_boundsHeight
+{
+    return CGRectGetHeight(self.bounds);
+}
+
+- (void)setQm_boundsHeight:(CGFloat)qm_boundsHeight
+{
+    if (self.qm_boundsHeight == qm_boundsHeight) {
+        return;
+    }
+    CGRect rect = self.bounds;
+    rect.size.height = qm_boundsHeight;
+    self.bounds = rect;
 }
 
 @end
